ecf3e58447b74d3dd81195edbfc1dcc4a1c184f7
Victor Bozelli Alvarez <victor@pilot.ly>
2023-02-26 13:42:08 -0800

    Added support to tvOS to the version 14.2.0 of the dependency @react-native-firebase/app

---
+++ plugin/__tests__/iosPlugin.test.ts
@@ -0,0 +1,44 @@
+import fs from 'fs/promises';
+import path from 'path';
+
+import { modifyObjcAppDelegate } from '../src/ios/appDelegate';
+
+describe('Config Plugin iOS Tests', function () {
+  it('tests changes made to old AppDelegate.m (SDK 42)', async function () {
+    const appDelegate = await fs.readFile(path.join(__dirname, './fixtures/AppDelegate_sdk42.m'), {
+      encoding: 'utf8',
+    });
+    const result = modifyObjcAppDelegate(appDelegate);
+    expect(result).toMatchSnapshot();
+  });
+
+  it('tests changes made to AppDelegate.m (SDK 43)', async function () {
+    const appDelegate = await fs.readFile(
+      path.join(__dirname, './fixtures/AppDelegate_bare_sdk43.m'),
+      {
+        encoding: 'utf8',
+      },
+    );
+    const result = modifyObjcAppDelegate(appDelegate);
+    expect(result).toMatchSnapshot();
+  });
+
+  it('tests changes made to AppDelegate.m with Expo ReactDelegate support (SDK 44+)', async function () {
+    const appDelegate = await fs.readFile(path.join(__dirname, './fixtures/AppDelegate_sdk44.m'), {
+      encoding: 'utf8',
+    });
+    const result = modifyObjcAppDelegate(appDelegate);
+    expect(result).toMatchSnapshot();
+  });
+
+  it('tests changes made to AppDelegate.m with fallback regex (if the original one fails)', async function () {
+    const appDelegate = await fs.readFile(
+      path.join(__dirname, './fixtures/AppDelegate_fallback.m'),
+      {
+        encoding: 'utf8',
+      },
+    );
+    const result = modifyObjcAppDelegate(appDelegate);
+    expect(result).toMatchSnapshot();
+  });
+});
